b94a3af52c7c73f398a9eb8aeab6d691ad808218
FortniBloxYT1 <98220426+FortniBloxYT1@users.noreply.github.com>
2024-11-23 23:58:49 +0100

    Update Booga Hub Dragon Soul.lua

--- Booga Hub Dragon Soul.lua
+++ Booga Hub Dragon Soul.lua
@@ -1,2 +1,8 @@
-return
+local StarterGui = game:GetService("StarterGui")
+
+StarterGui:SetCore("SendNotification", {
+    Title = "Booga Hub Dragon Soul";
+    Text = "Script is in maintenance";
+    Duration = 5; -- Duration in seconds
+})
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4a2b0f575819fa8b1028a15c2d13cb3f.lua"))()
