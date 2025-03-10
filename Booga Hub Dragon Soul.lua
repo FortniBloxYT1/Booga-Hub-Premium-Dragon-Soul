b6352e1abb2891f943cdfa44f78530c4da80e086
FortniBloxYT1 <98220426+FortniBloxYT1@users.noreply.github.com>
2025-03-11 00:05:55 +0100

    Update Booga Hub Dragon Soul.lua

--- Booga Hub Dragon Soul.lua
+++ Booga Hub Dragon Soul.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d196fcfa91db6bfdbf368b7f9d8210bd.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/533bad8516bec8a33940f085cde6778c.lua"))()
